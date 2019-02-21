4184297422bc82c2e132a92eee5b65b0783e4d3b
K-Kit <kylekitlinski@gmail.com>
2019-02-21 17:33:06 -0500

    Sandboxing

--- src/main/java/com/ExchangeDataLoader/BinanceDataLoader.java
+++ src/main/java/com/ExchangeDataLoader/BinanceDataLoader.java
@@ -7,16 +7,31 @@ import com.binance.api.client.domain.general.Asset;
 import com.binance.api.client.domain.market.BookTicker;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BinanceDataLoader {
     static BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance();
     static BinanceApiRestClient client = factory.newRestClient();
 
+    /*
+    * TODO learn how to java
+    * Currently sandboxing some code to adjust to java
+    * just using print statements in main() to figure out what I'm working with
+    */
     public static void main(String[] args) throws Exception {
         List<BookTicker> tickers = client.getBookTickers();
         for (BookTicker ticker: tickers
              ) {
-            System.out.println(ticker);
+            System.out.println(ticker.getSymbol());
         }
+        List<String> l;
+        /*
+        * Definitely didn't have arrow functions last time I wrote Java, Sweet!
+        *
+        * */
+        l = tickers.stream().filter(s->s.getSymbol().endsWith("ETH")).map(bookTicker -> {
+            return bookTicker.getSymbol();
+        }).collect(Collectors.toList());
+        System.out.println(l);
     }
 }
