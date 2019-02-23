bad881aea86bee4762c5c8b0c86d1d1f1486af61
K-Kit <kylekitlinski@gmail.com>
2019-02-23 17:00:49 -0500

    Forgot to start a new branch, websocket code started todos - add talib calcs, refactor db

--- src/main/java/com/DynamoManager.java
+++ src/main/java/com/DynamoManager.java
@@ -6,6 +6,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.*;
 import com.amazonaws.services.dynamodbv2.model.*;
+import com.binance.api.client.domain.event.CandlestickEvent;
 import com.binance.api.client.domain.market.Candlestick;
 
 import java.lang.reflect.Array;
@@ -43,6 +44,21 @@ public class DynamoManager {
                 .withDouble("volume", Double.parseDouble((candlestick.getVolume())));
     }
 
+    /*
+    * Think theres some way to not have to do 2 seperate funcs with generics maybe... will visit later
+    * Static type is nice but definitely a bit of a learning curve
+    * */
+    public Item createCandleEventItem(CandlestickEvent candlestick) {
+        return new Item().withPrimaryKey("timestamp", candlestick.getOpenTime())
+                .withDouble("open", Double.parseDouble((candlestick.getOpen())))
+                .withDouble("high", Double.parseDouble((candlestick.getHigh())))
+                .withDouble("low", Double.parseDouble((candlestick.getLow())))
+                .withDouble("close", Double.parseDouble((candlestick.getClose())))
+                .withDouble("volume", Double.parseDouble((candlestick.getVolume())));
+    }
+
+
+
 
     /*
      * naming convention is SYMBOL_INTERVAL
@@ -62,12 +78,12 @@ public class DynamoManager {
     * https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBMapper.BatchWriteExample.html
     * probably make a que of candlesticks as they come in from the websockets and batch add every 5sec
     * */
-    public void writeCandle(String symbol, String interval, Candlestick candlestick){
+    public void writeCandle(CandlestickEvent candlestick){
         /*
         * Insert a single candle into its table
-
         */
-
+        String symbol, interval;
+        Item item = createCandleEventItem(candlestick);
         /*
         * it seems inefficient to be caching these variables, if I were writing python I'd map a list of touples
         * or something into the db functions, may revisit this later don't want to waste time fixing something
@@ -76,13 +92,9 @@ public class DynamoManager {
         */
 
         // probably move this to its own function at some point
-        String tableName = getCandleTableName(symbol, interval);
+        String tableName = getCandleTableName(candlestick.getSymbol().toUpperCase(), candlestick.getIntervalId());
 
         Table table = dynamoDB.getTable(tableName);
-
-        Item item = createCandleItem(candlestick);
-
-
 
 
         // if the table doesn't exist create the table and put the item
@@ -97,14 +109,17 @@ public class DynamoManager {
 
     /*
     * Batch add entire candle history to avoid having to write 500 candles 1 at a time for 100+ pairs
+    * still slow but much improved
     * */
     public void writeHistoricalCandles(String symbol, String interval, List<Candlestick> candlesticks){
+        String tableName = getCandleTableName(symbol, interval);
+        createCandleTable(tableName);
         try {
             do {
-                TableWriteItems tableWriteItems = new TableWriteItems(getCandleTableName(symbol,interval));
+                TableWriteItems tableWriteItems = new TableWriteItems(tableName);
 
                 // max batch write items is 25 so we pop the candlesticks and pass in 24 at a time
-                for (int i=0; i <25; i++){
+                for (int i=0; i <= 25; i++){
                     if (candlesticks.isEmpty()) break;
                     tableWriteItems.addItemToPut(createCandleItem(candlesticks.remove(0)));
                 }
@@ -140,16 +155,21 @@ public class DynamoManager {
         * Timestamp is index, other values will be Open High Low Close Volume
         * TODO add error handling
         * */
-        Table table = dynamoDB.createTable(tableName,
-                Arrays.asList(new KeySchemaElement("timestamp", KeyType.HASH)),
-                Arrays.asList(new AttributeDefinition("timestamp", ScalarAttributeType.N)),
-                new ProvisionedThroughput(10L, 10L));
         try {
-            table.waitForActive();
-        } catch (InterruptedException e) {
+            Table table = dynamoDB.createTable(tableName,
+                    Arrays.asList(new KeySchemaElement("timestamp", KeyType.HASH)),
+                    Arrays.asList(new AttributeDefinition("timestamp", ScalarAttributeType.N)),
+                    new ProvisionedThroughput(10L, 10L));
+            try {
+                table.waitForActive();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Success.  Table status: " + table.getDescription().getTableStatus());
+        }
+        catch (Exception e){
             e.printStackTrace();
         }
-        System.out.println("Success.  Table status: " + table.getDescription().getTableStatus());
     }
 
 
